58fe9d908987141d7bb5aa7efa69eeae7a003a0b
Jacob Wiechmann <Jacob Wiechmann@10.0.66.143>
2019-10-06 14:39:27 -0500

    part 3 of the assignment completed, tests if earthquake was on land or not

--- UCSDUnfoldingMaps/src/module4/EarthquakeCityMap.java
+++ UCSDUnfoldingMaps/src/module4/EarthquakeCityMap.java
@@ -170,7 +170,8 @@ public class EarthquakeCityMap extends PApplet {
 		// If isInCountry ever returns true, isLand should return true.
 		for (Marker m : countryMarkers) {
 			// TODO: Finish this method using the helper method isInCountry
-			
+			isInCountry(earthquake,m); 
+			return true;
 		}
 		
 		
